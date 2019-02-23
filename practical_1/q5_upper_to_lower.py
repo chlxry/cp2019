3addc8e3d9975cec45aceb9d79d8848b8553bfc1
chlxry <47800441+chlxry@users.noreply.github.com>
2019-02-23 22:22:39 +0800

    Update q5_upper_to_lower.py

--- practical_1/q5_upper_to_lower.py
+++ practical_1/q5_upper_to_lower.py
@@ -1,6 +1,6 @@
 # Write a program q5_upper_to_lower.py that converts an uppercase letter from standard input to a lowercase letter 
 # by making use of its ASCII value
 
-upper = input("Enter word with uppercase here boiii: ")
+upper = input("Input word with uppercase here boiii: ")
 
 print("Word in lowercase: " + upper.lower())
