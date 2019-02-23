0884b685655cbf821ae2285d35ab933f3110a432
chlxry <47800441+chlxry@users.noreply.github.com>
2019-02-23 22:39:27 +0800

    Create q6_find_ascii_char.py

---
+++ practical_1/q6_find_ascii_char.py
@@ -0,0 +1,2 @@
+# Write a program q6_find_ascii_char.py that receives an ASCII code (an integer between 0 and 127) and displays its character
+# For example, if the user enters 97, the program displays character a
