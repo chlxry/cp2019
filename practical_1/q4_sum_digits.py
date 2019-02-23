7099ae4ca33760d9b459759803a6435dd0efcd6f
chlxry <47800441+chlxry@users.noreply.github.com>
2019-02-23 22:14:30 +0800

    Create q4_sum_digits.py

---
+++ practical_1/q4_sum_digits.py
@@ -0,0 +1,13 @@
+# Write a program q4_sum_digits.py that reads an integer between 0 and 1000 and adds all the digits in the integer
+# For example, if an integer is 932, the sum of all its digits is 14
+# Hint: Use the % operator to extract digits, and use the // operator to remove the extracted digit
+# For instance, 932 % 10 = 2 and 932 // 10 = 93 
+
+x = int(input("Enter a number boiii: "))
+sum = 0
+while (x > 0):
+    dig = x % 10
+    sum = sum + dig
+    x = x // 10
+    
+print("The sum of digits is: " + str(sum))
