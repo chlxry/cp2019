f31b88ac37c3d21deb8cd4da37e87b73aa56e291
chlxry <47800441+chlxry@users.noreply.github.com>
2019-02-23 23:20:53 +0800

    Update q6_find_ascii_char.py

--- practical_1/q6_find_ascii_char.py
+++ practical_1/q6_find_ascii_char.py
@@ -1,2 +1,5 @@
 # Write a program q6_find_ascii_char.py that receives an ASCII code (an integer between 0 and 127) and displays its character
 # For example, if the user enters 97, the program displays character a
+
+ascii = eval(input("Input ASCII code here boiii: "))
+print("Corresponding character: " + chr(ascii))
