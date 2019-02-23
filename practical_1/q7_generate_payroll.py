61bcb2f17236aa7f24fab73add55fda75d30c14c
chlxry <47800441+chlxry@users.noreply.github.com>
2019-02-24 00:00:39 +0800

    Update q7_generate_payroll.py

--- practical_1/q7_generate_payroll.py
+++ practical_1/q7_generate_payroll.py
@@ -1,17 +1,27 @@
-Write a program q7_generate_payroll.py that reads the following information and prints a payroll statement. A sample input and output session is as follows:
+# Write a program q7_generate_payroll.py that reads the following information and prints a payroll statement
 
-Sample input:
+# Sample input:
+# Enter name: Lim Ah Seng
+# Enter number of hours worked weekly: 10
+# Enter hourly pay rate: 6.75
+# Enter CPF contribution rate(%): 20
 
-Enter name: Lim Ah Seng
-Enter number of hours worked weekly: 10
-Enter hourly pay rate: 6.75
-Enter CPF contribution rate(%): 20
+# Sample output:
+# Payroll statement for Lim Ah Seng
+# Number of hours worked in week: 10
+# Hourly pay rate: $6.75
+# Gross pay = $67.50
+# CPF contribution at 20% = $13.50
+# Net pay = $54.00
 
-Sample output:
+name = str(input("Enter name: "))
+hours = float(input("Enter number of hours worked weekly: "))
+rate = float(input("Enter hourly pay rate: "))
+cpf = float(input("Enter CPF contribution rate(%)"))
 
-Payroll statement for Lim Ah Seng
-Number of hours worked in week: 10
-Hourly pay rate: $6.75
-Gross pay = $67.50
-CPF contribution at 20% = $13.50
-Net pay = $54.00
+print("Payroll statement for " + name)
+print("Number of hours worked in week: " + str(hours))
+print("Hourly pay rate: $" + "{0:.2f}".format(rate))
+print("Gross pay = $" + ("{0:.2f}".format(rate) * hours)) # can't multiply sequence by non-int of type 'float'
+print("CPF contribution at 20% = $" + ("{0:.2f}".format(rate) * hours) / 100 * 20)
+print("Net pay = $" + (("{0:.2f}".format(rate) * hours) - ("{0:.2f}".format(rate) * hours) / 100 * 20))
