28147de97a28e03fce0f7f6430ec36813c620a13
chlxry <47800441+chlxry@users.noreply.github.com>
2019-02-23 23:22:03 +0800

    Create q7_generate_payroll.py

---
+++ practical_1/q7_generate_payroll.py
@@ -0,0 +1,17 @@
+Write a program q7_generate_payroll.py that reads the following information and prints a payroll statement. A sample input and output session is as follows:
+
+Sample input:
+
+Enter name: Lim Ah Seng
+Enter number of hours worked weekly: 10
+Enter hourly pay rate: 6.75
+Enter CPF contribution rate(%): 20
+
+Sample output:
+
+Payroll statement for Lim Ah Seng
+Number of hours worked in week: 10
+Hourly pay rate: $6.75
+Gross pay = $67.50
+CPF contribution at 20% = $13.50
+Net pay = $54.00
