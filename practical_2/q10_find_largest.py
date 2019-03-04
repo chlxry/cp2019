c84e8b6178b53958bba7785a7b5dedea1ae8057c
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-05 01:03:32 +0800

    Create q10_find_largest.py

---
+++ practical_2/q10_find_largest.py
@@ -0,0 +1,11 @@
+# Use a while loop to find the largest integer n such that n3 is less than 12,000.
+
+n = 10
+
+while n < 12000 ** (1/3):
+  if n**3 < 12000:
+    n = n + 1
+  else:
+    break
+    
+print("The largest integer n such that n^3 is less than 12,000 is " + str(int(n-1))+ ".")
