e87e0d5055aa0e13c7e715f37cb565bf6fa37edf
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-05 21:26:48 +0800

    Update q12_find_factors.py

--- practical_2/q12_find_factors.py
+++ practical_2/q12_find_factors.py
@@ -1,4 +1,5 @@
 # Write a program that reads an integer and displays all its smallest factors. 
 # For example, if the input integer is 120, the output should be as follows: 2, 2, 2, 3, 5.
 
-x = int
+x = int(input("enter integer here: "))
+
