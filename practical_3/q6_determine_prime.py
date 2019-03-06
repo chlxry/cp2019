2b13d6bdd777c1a07e8876eec4f256ec7a4a2605
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-06 23:28:46 +0800

    Create q6_determine_prime.py

---
+++ practical_3/q6_determine_prime.py
@@ -0,0 +1,11 @@
+# Write a function to determine whether an integer is a prime number. 
+# An integer greater than 1 is a prime number if its only divisor is 1 or itself. 
+# For example, is_prime(11) returns True, and is_prime(9) returns False.
+
+# Use the is_prime(n) function to find the first thousand prime numbers and display every ten prime numbers in a row, as follows:
+
+# 2   3   5   7   11   13   17   19   23   29
+# 31  37  41  43  47   53   59   61   67   71
+# 73  79  83  89  97  ...
+# ...
+
