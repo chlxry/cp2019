4657a454519cf33a79cb5f044e26b49c1d0a2e7d
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-06 09:24:36 +0800

    Create q3_find_gcd.py

---
+++ practical_3/q3_find_gcd.py
@@ -0,0 +1,5 @@
+# Write a function gcd(m, n) that returns the greatest common divisor between two positive integers:
+
+# Write a test program that computes gcd(24, 16) and gcd(255, 25).
+
+def gcd(m, n):
