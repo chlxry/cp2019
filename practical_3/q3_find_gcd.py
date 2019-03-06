da028c79cbbb85c4ac901679b21d4a0083266291
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-06 10:52:58 +0800

    Update q3_find_gcd.py

--- practical_3/q3_find_gcd.py
+++ practical_3/q3_find_gcd.py
@@ -2,4 +2,13 @@
 
 # Write a test program that computes gcd(24, 16) and gcd(255, 25).
 
-def gcd(m, n):
+def gcd(m, n): 
+    if n == 0: 
+        return m 
+    else: 
+        return gcd(n , m % n) 
+  
+m = int(input("enter integer here: "))
+n = int(input("enter integer here: "))
+  
+print ("the greatest common divisor of " + str(m) + " and " + str(n) + " is " + str(gcd(m, n))) 
