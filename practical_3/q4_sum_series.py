63805469f50038faa4edf2f3dfe147767b3aa8e9
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-06 22:02:12 +0800

    Create q4_sum_series.py

---
+++ practical_3/q4_sum_series.py
@@ -0,0 +1,11 @@
+# Write a function m_series(i) to compute the following series:
+
+# Write a test program that displays the following table:
+
+# i         m(i)     
+# 1         0.5000   
+# 2         1.1667   
+# ... 
+# 19        16.4023  
+# 20        17.3546  
+
