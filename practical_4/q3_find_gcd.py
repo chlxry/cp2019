552a8804716de9876cbe0e1eec30529a055faa9c
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-16 16:28:30 +0800

    Create q3_find_gcd.py

---
+++ practical_4/q3_find_gcd.py
@@ -0,0 +1,6 @@
+# The greatest common divisor (GCD) of two positive integers m and n, gcd(m, n) can be defined recursively as follows: 
+
+# If m % n is 0, gcd(m, n) is n.
+# Otherwise, gcd(m, n) is gcd(n, m % n).
+
+# Write a recursive function gcd(m, n) to find the GCD. Write a test program that computes gcd(24, 16) and gcd(255, 25).
