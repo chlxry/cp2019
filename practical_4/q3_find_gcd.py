a2dbd38395bbef3dd22da3a6ccc74b8bf0d0169c
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-16 16:40:33 +0800

    Update q3_find_gcd.py

--- practical_4/q3_find_gcd.py
+++ practical_4/q3_find_gcd.py
@@ -4,3 +4,16 @@
 # Otherwise, gcd(m, n) is gcd(n, m % n).
 
 # Write a recursive function gcd(m, n) to find the GCD. Write a test program that computes gcd(24, 16) and gcd(255, 25).
+
+def gcd(m, n): 
+    if n == 0: 
+        return m 
+    else: 
+        return gcd(n , m % n) 
+      
+# test program
+gcd(24, 16) # input
+8 # output
+
+gcd(255, 25) # input
+5 # output
