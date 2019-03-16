49ddf51701a550e41bb49bd40631afd337119aaa
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-16 16:52:32 +0800

    Update q2_sum_series2.py

--- practical_4/q2_sum_series2.py
+++ practical_4/q2_sum_series2.py
@@ -1,9 +1,7 @@
-def sum_series2(i):
-  i = int(input("enter 'i': "))
+def sum_series2(i): 
   n = 1
   list = []
   while n <= i:
       list.append(n / (2 * n + 1))
       n = n + 1
-    
   print(sum(list))
