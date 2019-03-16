617dbf3e5c1ff49036ed9fb6814e35c18636b9ba
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-16 21:17:10 +0800

    Create q7_find_largest.py

---
+++ practical_4/q7_find_largest.py
@@ -0,0 +1,6 @@
+# Write a recursive function find_largest(alist) that returns the largest integer in an array alist.  
+
+# For example, given alist = [5, 1, 8, 7, 2], sum_digits(alist) returns 8.
+
+def find_largest(alist):
+    print(max(alist))
