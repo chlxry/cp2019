556285afbfed32b0f6788ae20147cb4f78851170
chlxry <47800441+chlxry@users.noreply.github.com>
2019-03-16 20:09:11 +0800

    Create q5_count_letter.py

---
+++ practical_4/q5_count_letter.py
@@ -0,0 +1,7 @@
+# Write a recursive function count_letter(str, ch) that finds the number of occurrences of a specified letter ch in a string str: 
+
+# For example, count_letter("Welcome", 'e') returns 2.
+
+def count_letter(str, ch):
+    str = str
+    print(str.count(ch))
